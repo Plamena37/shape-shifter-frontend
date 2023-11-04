4cbb34123f3e1cd24f5d5f3b2df187e13bb90b0b
Plamena <yomena37@gmail.com>
2023-11-04 17:34:15 +0200

    Add action type prefix enums

--- src/utils/common-enums.tsx
+++ src/utils/common-enums.tsx
@@ -37,3 +37,22 @@ export enum EXERCISETYPE {
   RHOMBOIDS = "rhomboids",
   SHOULDERS = "shoulders",
 }
+
+export enum UsersSliceActionTypePrefix {
+  USERS_CREATE = "users/signup",
+  USERS_LOGIN = "users/login",
+  USERS_GET_ALL = "users/getAllUsers",
+  USERS_GET_ONE = "users/getUserById",
+  USERS_UPDATE = "users/updateUserById",
+  USERS_UPDATE_ROLE = "users/updateRole",
+  USERS_DELETE = "users/deleteUser",
+}
+
+export enum ExerciseTypesSliceActionTypePrefix {
+  EXERCISE_TYPES_CREATE = "exerciseTypes/createExerciseType",
+  EXERCISE_TYPES_GET_ALL = "exerciseTypes/getAllExerciseTypes",
+  EXERCISE_TYPES_GET_ONE = "exerciseTypes/getExerciseTypeById",
+  EXERCISE_TYPES_UPDATE = "exerciseTypes/updateExerciseTypeById",
+  EXERCISE_TYPES_DELETE = "exerciseTypes/deleteExerciseType",
+  EXERCISE_TYPES_FILTER = "exerciseTypes/filterExerciseTypes",
+}
