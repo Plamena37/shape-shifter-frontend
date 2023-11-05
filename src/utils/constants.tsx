986e2887c7df93d5f749a72179b90d2538bd9519
Plamena <yomena37@gmail.com>
2023-11-05 17:39:40 +0200

    Code cleanup

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,1 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API;
+// export const API_BASE_URL = import.meta.env.VITE_API;
+export const API_BASE_URL =
+  "https://shape-shifter-api-e346a560a04f.herokuapp.com";
